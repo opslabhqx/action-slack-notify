0be081efbc609c6a9425b7b12669dd1a48c57e44
Anthony Yung <ycya88@gmail.com>
2024-07-23 02:26:48 +0800

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# action-slack-notify
-A GitHub Action for sending notifications to Slack 
+# action-slacknotify
+
+[![action-slacknotify](https://img.shields.io/github/v/release/opslabhqx/action-slacknotify.svg)](https://github.com/opslabhqx/action-slacknotify/releases)
+
+Send notifications seamlessly to Slack from your GitHub Actions workflows with `action-slacknotify`. This GitHub Action allows you to notify your team about the success, failure, or any other status of your GitHub Actions workflows directly into Slack.
+
+## Getting Started
+
+### Usage
+
+To use this action, add the following step to the end of your job in your GitHub workflow YAML file. Ensure that you add this step after any other steps that you want to monitor with notifications.
+
+```yaml
+    - name: Run Slack Notification
+      if: always()  # Ensures notification is sent regardless of previous step success or failure
+      uses: opslabhqx/action-slacknotify@v1 # Check https://github.com/opslabhqx/action-slacknotify/releases for latest release
+      with:
+        webhook_url: ${{ secrets.SLACK_WEBHOOK_URL }}
+```
+
+### Obtaining Slack App Webhook URL
+
+To use this action, you need to obtain a webhook URL from Slack. Follow these steps:
+
+1. **Create a Slack App:**
+   - Go to the [Slack API: Applications](https://api.slack.com/apps) page.
+   - Click on the "Create New App" button.
+   - Enter an App Name and choose the workspace where you want to post notifications.
+   - Click "Create App".
+
+2. **Add Incoming Webhooks:**
+   - In your newly created app, navigate to "Incoming Webhooks" on the left sidebar.
+   - Activate the "Incoming Webhooks" toggle.
+   - Click "Add New Webhook to Workspace".
+   - Select the channel where you want to send notifications and click "Authorize".
+
+3. **Copy the Webhook URL:**
+   - After authorizing, you will see a new webhook URL generated.
+   - Copy this URL and add it to your GitHub repository secrets as `SLACK_WEBHOOK_URL`.
+
+### Example Workflow
+
+```yaml
+name: Example CI Workflow
+
+on: [push]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Checkout Code
+      uses: actions/checkout@v2
+
+    - name: Build Project
+      run: make build
+
+    - name: Run Slack Notification
+      if: always()
+      uses: opslabhqx/action-slacknotify@v1
+      with:
+        webhook_url: ${{ secrets.SLACK_WEBHOOK_URL }}
+```
+
+## License
+
+This repository is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
---
+++ notify_slack.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# This script is specifically designed for the GitHub Action opslabhqx/slacknotify-action@v1.
+# Running it outside of GitHub Actions may result in dependency issues. Use at your own risk.
+
+set -o errexit -o pipefail
+
+function define_var_required() {
+    local variables="$@"
+    for var in $variables; do
+        if [ -n "${!var}" ]; then
+            echo "$var=${!var}"
+        else
+            echo "Error: $var is not set" >&2
+            exit 1
+        fi
+    done
+}
+
+function define_var_optional() {
+    local variables="$@"
+    for var in $variables; do
+        if [ -n "${!var}" ]; then
+            echo "$var=${!var}"
+        else
+            echo "Warning: $var is not set" >&2
+        fi
+    done
+}
+
+function check_notify_slack() {
+    echo "Checking required variables..."
+    define_var_required "GITHUB_JOB_STATUS" "SLACK_WEBHOOK_URL" "GITHUB_WORKFLOW" "GITHUB_REF" "GITHUB_RUN_URL" "GITHUB_REPOSITORY_NAME" "GITHUB_REPOSITORY_URL" "GITHUB_ACTOR"
+    define_var_optional "GITHUB_COMMIT_MESSAGE" # Does not work on workflow_dispatch
+}
+
+function send_notify_slack() {
+    local webhook_url="${SLACK_WEBHOOK_URL:-N/A}"
+    local status="${GITHUB_JOB_STATUS:-N/A}"
+    local action_name="${GITHUB_WORKFLOW:-N/A}"
+    local emoji
+    local prefix
+    local message
+
+    if [ "$status" == "success" ]; then
+        message="Deployment *succeeded* for project <$GITHUB_REPOSITORY_URL|$GITHUB_REPOSITORY_NAME>"
+        emoji=":white_check_mark:"
+    else
+        message="Deployment *failed* for project <$GITHUB_REPOSITORY_URL|$GITHUB_REPOSITORY_NAME>"
+        emoji=":x:"
+    fi
+    prefix="${emoji} *${action_name}*"
+    local json_payload
+    json_payload=$(jq -n \
+        --arg prefix "$prefix" \
+        --arg message "$message" \
+        --arg committer "*Committer:* ${GITHUB_ACTOR:-N/A}" \
+        --arg branch_tag "*Branch/Tag:* ${GITHUB_REF:-N/A}" \
+        --arg pipeline_url "*Pipeline URL:* <${GITHUB_RUN_URL:-about:blank}|View Pipeline>" \
+        --arg commit_msg "*Commit Message:* ${GITHUB_COMMIT_MESSAGE:-N/A}" \
+        '{ "blocks": [ { "type": "header", "text": { "type": "plain_text", "text": $prefix } }, { "type": "section", "text": { "type": "mrkdwn", "text": $message } }, { "type": "context", "elements": [ { "type": "mrkdwn", "text": $committer }, { "type": "mrkdwn", "text": $branch_tag }, { "type": "mrkdwn", "text": $pipeline_url } ] }, { "type": "context", "elements": [ { "type": "mrkdwn", "text": $commit_msg } ] } ] }')
+    curl -s -X POST -H 'Content-type: application/json' --data "$json_payload" "$webhook_url" || { echo "Error: Failed to send Slack notification" >&2; exit 1; }
+}
+
+function send() {
+    check_notify_slack
+    send_notify_slack
+}
+
+if [ "$#" -eq 1 ]; then
+    "$1"
+else
+    echo "Usage: $0 <function_name>" >&2
+    exit 1
+fi
